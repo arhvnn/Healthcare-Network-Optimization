4bb17ea81b6b25135997d3fd404e09b6ffe4b62b
29akram25 <146698067+29akram25@users.noreply.github.com>
2024-04-26 14:56:20 +0100

    Update solver.py :: added DFS

--- src/solver.py
+++ src/solver.py
@@ -32,3 +32,24 @@ class Solver:
             path.insert(0, node)
             node = node.parent
         return path
+
+     def depth_first_search(self):
+        initial_node = Node(state=self.problem.initial_state, path_cost=0)
+        if self.problem.goal_test(initial_node.state):
+            return self.solution(initial_node)
+
+        frontier = [initial_node]
+        explored = set()
+
+        while frontier:
+            node = frontier.pop()
+            explored.add(node.state)
+
+            for action in self.problem.actions(node.state):
+                child = self.problem.child_node(node, action)
+                if child.state not in explored and child not in frontier:
+                    if self.problem.goal_test(child.state):
+                        return self.solution(child)
+                    frontier.append(child)
+
+        return "GOAL IS UNREACHABLE!"
